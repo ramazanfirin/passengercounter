1b5495a65ce05764072fd35e86946eba5c1f6431
heskontrolsistemleri <ramazan_firin@hotmail.com>
2021-10-15 22:08:19 +0000

    initial

---
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('passengercounter2App')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
